f0a562d3fe81e574c8399e02a78fbb0b0c619dc6
Kyle Hearty <kgh11@pitt.edu>
2015-04-07 19:24:28 -0400

    testing

--- TrainController/TrainControllerWrapper.java
+++ TrainController/TrainControllerWrapper.java
@@ -39,7 +39,7 @@ public class TrainControllerWrapper {
 	public void tick() {
 //		this.temperature = simulator.getTemp();
 //		this.systemTime = simulator.getTime();
-		for(int i = 1; i < controllers.size() + 1; i++) {
+		for(int i = 0; i < controllers.size(); i++) {
 			controllers.get(i).tick(0, 0);
 		}
 		ui.update();
