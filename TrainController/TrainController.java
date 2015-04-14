417becfa67a6f4a2f405b15cc5d062676e59e8d2
Kyle Hearty <kgh11@pitt.edu>
2015-04-14 12:39:09 -0400

    returned to old power calculations for now

--- TrainController/TrainController.java
+++ TrainController/TrainController.java
@@ -19,20 +19,39 @@ public class TrainController {
 	
 	private TrainModel			model;
 	private TrainControllerUI	ui;
-	private VitalControl		vc;
+	//private VitalControl		vc;
+	
+	//REMOVE WHEN VITAL
+	private final int		MAX_POWER = 120000;			//Maximum power output (W)
+	private final double	MAX_VELOCITY = 19.4444;		//Maximum train velocity (m/s)
+	private final double	TIME_PERIOD = 1;			//Sample period of time (s)
+	private	final double	Ki = 0.00000001;			//Integral Gain
+	private	final double	Kp = 50;					//Proportional Gain
+	
+	private	double[]	powerCommand = new double[2];	//Current[1] and most recent[0] power commands
+	private double[]	u = new double[2];				//Intermediate variables
+	private double[]	velocityError = new double[2];	//Current [1] and most recent [0] velocity velocityError
 	
 	
 	public TrainController(int newID, TrainModel newTrainModel, TrainControllerUI newUI) {
 		ID = newID;
 		model = newTrainModel;
 		ui = newUI;
-		vc = new VitalControl();
+		//vc = new VitalControl();
 		mode = 1;
 		setpointVelocity = 0.0;
 		velocityFeedback = 0.0;
 		remainingAuthority = 0.0;
 		brakeStatus = false;
 		eBrakeStatus = false;
+		
+		//REMOVE WHEN VITAL
+		powerCommand[0] = 0.0;
+		powerCommand[1] = 0.0;
+		velocityError[0] = 0.0;
+		velocityError[1] = 0.0;
+		u[0] = 0.0;
+		u[1] = 0.0;
 		
 		//TEST
 		//remainingAuthority = 1000.0;
@@ -84,8 +103,31 @@ public class TrainController {
 		}
 		return;
 	}
+	
+	//REMOVE WHEN VITAL
+	public void calculatePower() {
 		
-
+		velocityFeedback = model.getVelocity();
+		
+		velocityError[1] = targetVelocity - velocityFeedback;
+		
+		if(powerCommand[1] < MAX_POWER) {
+			u[1] = u[0] + (TIME_PERIOD/2) * (velocityError[1] + velocityError[0]);
+		}
+		else {
+			u[1] = u[0];
+		}
+		
+		powerCommand[1] = Kp * velocityError[1] + Ki * u[1];
+			
+		u[0] = u[1];
+		powerCommand[0] = powerCommand[1];
+		velocityError[0] = velocityError[1];
+		
+		model.setPower(powerCommand[1]);
+		
+		return;
+	}
 	
 	public void setTargetVelocity(double newVelocity) {
 		targetVelocity = newVelocity;
@@ -93,7 +135,9 @@ public class TrainController {
 	
 	public void stopTrain() {
 		model.setPower(0);
-		vc.resetPower();
+		//CHANGE WHEN VITAL
+		powerCommand[0] = 0.0;
+		//vc.resetPower();
 		model.activateServiceBrakes();
 		brakeStatus = true;
 		return;
@@ -101,7 +145,9 @@ public class TrainController {
 	
 	public void emergencyStop() {
 		model.setPower(0);
-		vc.resetPower();
+		//CHANGE WHEN VITAL
+		powerCommand[0] = 0.0;
+		//vc.resetPower();
 		model.activateEmergencyBrakes();
 		eBrakeStatus = true;
 		return;
@@ -129,7 +175,9 @@ public class TrainController {
 			setTargetVelocity(setpointVelocity);
 		}
 		if(!brakeStatus && !eBrakeStatus) {
-			model.setPower(vc.vitalPower(model.getVelocity(), model.getVelocity(), model.getVelocity()));
+			//CHANGE WHEN VITAL
+			calculatePower();
+			//model.setPower(vc.vitalPower(model.getVelocity(), model.getVelocity(), model.getVelocity()));
 		}
 		
 		//controlSubsystems(currentTime, currentTemp);
